098178d49dac082590c80d150282ef8c3ace5a01
DiegoLibonati <diego.libonati1998@gmail.com>
2022-05-30 11:47:28 -0300

    fondo.gif preload

--- assets/loadParticles.js
+++ assets/loadParticles.js
@@ -1,3 +1,6 @@
 particlesJS.load('particles-js', 'assets/particles.js/demo/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+    console.log('Todo cargo perfecto!');
   });
+
+let image = new Image();
+image.src = 'https://diegolibonati.github.io/DiegoLibonatiWeb/templates/fondo.gif';
