584b328ab67f78eed97eb199af3373c018639dcf
DiegoLibonati <diego.libonati1998@gmail.com>
2022-06-27 10:52:21 -0300

    update

--- assets/intersectionObserverConfig.js
+++ assets/intersectionObserverConfig.js
@@ -2,6 +2,10 @@ const sectionSkills = document.getElementById("skills");
 const sectionAbout = document.getElementById("about");
 const sectionPortfolio = document.getElementById("portfolio");
 const sectionContact = document.getElementById("contact");
+const sectionHome = document.getElementById("home");
+const navLogo = document.getElementById("nav_logo");
+const sectionWorks = document.getElementById("works");
+
 
 const loadSections = (entrys, observador) =>{
 
@@ -9,11 +13,15 @@ const loadSections = (entrys, observador) =>{
 
         if (entry.isIntersecting){
 
-            setTimeout(()=>{
-
-                entry.target.classList.add("show-section");
-
-            }, 250);
+            if (entry.target.id == "home" || entry.target.id == "nav_logo"){
+                entry.target.style.opacity = "1"
+            }else if (entry.target.id == "skills" || entry.target.id == "portfolio"){
+                entry.target.style.transform = "scale(1.0)";
+            }else if (entry.target.id == "about" || entry.target.id == "contact"){
+                entry.target.style.transform = "translateX(0%)";
+            }else if (entry.target.id == "works"){
+                entry.target.style.transform = "scaleX(1.0)";
+            }
 
         }
 
@@ -33,3 +41,6 @@ observer.observe(sectionSkills);
 observer.observe(sectionAbout);
 observer.observe(sectionPortfolio);
 observer.observe(sectionContact);
+observer.observe(sectionHome);
+observer.observe(navLogo);
+observer.observe(sectionWorks);
